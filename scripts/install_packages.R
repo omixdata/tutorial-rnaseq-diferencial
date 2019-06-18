f100a74e71d11dcc47bb458b34eef9fdf65cb664
fredericokremer <fred.s.kremer@hotmail.com>
2019-06-18 15:52:23 -0300

    update on R library install script

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -2,5 +2,5 @@
 #!/usr/bin/env Rscript
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+    install.packages("BiocManager", repos='http://cran.us.r-project.org')
 BiocManager::install(c("DESeq","DESeq2","edgeR", "HTSFilter", "limma"),  dependencies=TRUE)
