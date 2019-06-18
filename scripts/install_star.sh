8bbdeeb2914d38569ee4e15443c4f2a089c4e546
fredericokremer <fred.s.kremer@hotmail.com>
2019-06-18 15:40:09 -0300

    install STAR updated

--- scripts/install_star.sh
+++ scripts/install_star.sh
@@ -1,6 +1,11 @@
 cd /tmp/
-mkdir star-aligner
+mkdir -p star-aligner
 cd star-aligner
+
+if [ -d STAR ]; then
+	rm -r STAR
+fi
+
 git clone https://github.com/alexdobin/STAR/
 chmod +x STAR/bin/Linux_x86_64/*
 cp STAR/bin/Linux_x86_64/* /usr/local/bin/
